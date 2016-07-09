f734f5acbba8886cf06134597c647a8ccad0954e
gyankov <g.yankov951@abv.bg>
2016-07-09 10:04:25 +0300

    Add files via upload

---
+++ 1/Element.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLRenderer
+{
+    public class Element : IElement
+    {
+        private IEnumerable<IElement> childElements;
+       // private string name;
+        //private string textContent;
+
+             
+        public Element(string name, string textContent):this(name)
+        {
+           
+            this.TextContent = textContent;
+
+        }
+
+        public Element(string name)
+            
+        {
+            this.Name = name;
+            this.childElements = new List<IElement>();
+
+        }
+
+
+
+        public virtual IEnumerable<IElement> ChildElements
+        {
+            get
+            {
+                return new List<IElement>(this.childElements);
+
+            }
+        }
+
+        public string Name { get; private set; }
+
+        public virtual string TextContent { get; set; }
+
+        public virtual  void AddElement(IElement element)
+        {
+            if (element==null)
+            {
+                throw new ArgumentNullException(); 
+            }
+
+            ((ICollection<IElement>)this.childElements).Add(element);
+        }
+
+        public virtual void Render(StringBuilder output)
+        {
+            if (!string.IsNullOrWhiteSpace(this.Name))
+            {
+                output.AppendFormat("<{0}>", this.Name);
+
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.TextContent))
+            {
+
+                foreach (var item in this.TextContent)
+                {
+                    switch (item)
+                    {
+                        case '<': output.Append("&lt;");break;
+                        case '>': output.Append("&gt;"); break;
+
+                        case '&': output.Append("&amp;"); break;
+
+
+
+                        default:
+                            break;
+                    }
+                }
+
+                output.Append( this.TextContent);
+
+            }
+
+            foreach (var item in this.childElements)
+            {
+                output.Append(item.ToString());
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.Name))
+            {
+                output.AppendFormat("</{0}>", this.Name);
+
+            }
+        }
+
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+
+            this.Render(output);
+
+            return output.ToString();
+        }
+    }
+}
---
+++ 1/Table.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLRenderer
+{
+    class Table : Element, ITable
+    {
+
+        private int rows;
+        private int cols;
+        private IElement[,] cells;
+
+
+        public Table(int rows, int cols)
+            :base("table",null)
+        {
+            this.Cols = cols;
+            this.Rows = rows;
+            this.cells = new IElement[this.Rows,this.Cols];
+        }
+
+
+
+        public IElement this[int row, int col]
+        {
+            get
+            {
+                ValidIndeces(row, col);
+                return this.cells[row, col];
+            }
+
+            set
+            {
+                ValidIndeces(row, col);
+                if (value==null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                this.cells[row, col] = value;
+            }
+        }
+
+        public int Cols
+        {
+            get
+            {
+                return this.cols;
+            }
+            private set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.cols = value;
+            }
+        }
+
+        public int Rows
+        {
+            get
+            {
+                return this.rows;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.rows = value;
+            }
+        }
+
+        private void ValidIndeces(int row, int col)
+        {
+            if (row<0 || col<0 || row>=this.Rows || col>=this.Cols )
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public override void AddElement(IElement element)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override IEnumerable<IElement> ChildElements
+        {
+            get
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public override string TextContent
+        {
+            get
+            {
+                throw new InvalidOperationException();
+            }
+
+            set
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public override void Render(StringBuilder output)
+        {
+
+            output.AppendFormat("<{0}>", this.Name);
+
+            for (int row = 0; row < this.Rows; row++)
+            {
+                output.AppendFormat("<tr>");
+
+                for (int col = 0; col < this.Cols; col++)
+                {
+                    output.Append("<td>");
+
+                    output.Append(this.cells[row, col].ToString());
+
+                    output.Append("</td>");
+
+                }
+
+                output.AppendFormat("</tr>");
+
+
+            }
+
+
+            output.AppendFormat("<{/0}>", this.Name);
+
+
+        }
+    }
+}
